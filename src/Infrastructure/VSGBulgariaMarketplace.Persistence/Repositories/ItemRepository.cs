bc6bda5f82a630299e21f09fcec4e8ae304ff185
Veselin Denchev <VDenchev@vsgbg.com>
2023-05-05 15:58:58 +0300

    Create ItemRepository.cs
    I misclicked and the Item repository wasn't included in the previous commit.

---
+++ src/Infrastructure/VSGBulgariaMarketplace.Persistence/Repositories/ItemRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace VSGBulgariaMarketplace.Persistence.Repositories
+{
+    using VSGBulgariaMarketplace.Application.Models.Item.Interfaces;
+    using VSGBulgariaMarketplace.Application.Models.UnitOfWork;
+    using VSGBulgariaMarketplace.Domain.Entities;
+
+    public class ItemRepository : Repository<Item, int>, IItemRepository
+    {
+        public ItemRepository(IUnitOfWork unitOfWork)
+            : base(unitOfWork)
+        {
+            this.columnNamesString = base.columnNamesString.Replace("Category", "CategoryId")
+                                                            .Replace("Orders", "OrderId");
+
+            this.parameterizedColumnsNamesString = base.columnNamesString.Replace("@Category", "@CategoryId")
+                                                                            .Replace("@Orders", "@OrderId");
+
+            base.insertSqlCommand = $"INSERT INTO Orders {this.columnNamesString} " +
+                                    $"VALUES {base.parameterizedColumnsNamesString} ";
+        }
+    }
+}
