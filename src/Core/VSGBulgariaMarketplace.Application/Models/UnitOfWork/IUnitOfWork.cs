b570a790fa74ec1fa8f47a97443312d2c0045cfc
Veselin Denchev <VDenchev@vsgbg.com>
2023-05-05 11:11:02 +0300

    Implement Unit of work pattern

---
+++ src/Core/VSGBulgariaMarketplace.Application/Models/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿namespace VSGBulgariaMarketplace.Application.Models.UnitOfWork
+{
+    using System.Data;
+
+    public interface IUnitOfWork
+    {
+        public IDbConnection DbConnection { get; set; }
+
+        public IDbTransaction Transaction { get; set; }
+
+        public void Begin();
+
+        public void Rollback();
+
+        void Commit();
+
+        void Dispose();
+    }
+}
---
+++ src/Infrastructure/VSGBulgariaMarketplace.Persistence/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,51 @@
+﻿namespace VSGBulgariaMarketplace.Persistence.UnitOfWork
+{
+    using Microsoft.Data.SqlClient;
+    using Microsoft.Extensions.Configuration;
+
+    using System.Data;
+
+    using VSGBulgariaMarketplace.Application.Models.UnitOfWork;
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        public UnitOfWork(IConfiguration configuration)
+        {
+            this.DbConnection = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+            this.DbConnection.Open();
+        }
+
+        public IDbConnection DbConnection { get; set; }
+
+        public IDbTransaction Transaction { get; set; }
+
+        public void Begin()
+        {
+            this.Transaction = this.DbConnection.BeginTransaction();
+        }
+        public void Commit()
+        {
+            this.Transaction.Commit();
+            this.Dispose();
+        }
+
+        public void Dispose()
+        {
+            if (this.Transaction != null)
+            {
+                this.Transaction.Dispose();
+                this.Transaction = null;
+            }
+            if (this.DbConnection != null)
+            {
+                this.DbConnection.Dispose();
+                this.DbConnection = null;
+            }
+        }
+        public void Rollback()
+        {
+            this.Transaction.Rollback();
+            this.Dispose();
+        }
+    }
+}
