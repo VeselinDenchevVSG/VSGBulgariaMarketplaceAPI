75741b4af64e99493326e276a6cddc793c3aa1d1
Veselin Denchev <VDenchev@vsgbg.com>
2023-05-05 11:32:41 +0300

    Implement generic repository

---
+++ src/Infrastructure/VSGBulgariaMarketplace.Persistence/Repositories/Abstraction/Interfaces/IRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace VSGBulgariaMarketplace.Persistence.Repositories.Abstraction.Interfaces
+{
+    using System.Data;
+
+    using VSGBulgariaMarketplace.Domain.Entities;
+
+    public interface IRepository<T, U> where T : BaseEntity<U>
+    {
+        public IDbTransaction Transaction { get; }
+
+        T[] GetAll();
+
+        T GetById(U id);
+
+        void Create(T entity);
+
+        void CreateMany(T[] entities);
+
+        void Update(U id, T entity);
+
+        void Delete(U id);
+
+        void DeleteMany(U[] ids);
+    }
+}
---
+++ src/Infrastructure/VSGBulgariaMarketplace.Persistence/Repositories/Abstraction/Repository.cs
@@ -0,0 +1,163 @@
+﻿namespace VSGBulgariaMarketplace.Persistence.Repositories.Abstraction
+{
+    using Dapper;
+
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Linq;
+    using System.Text;
+
+    using VSGBulgariaMarketplace.Application.Models.UnitOfWork;
+    using VSGBulgariaMarketplace.Domain.Entities;
+
+    using VSGBulgariaMarketplace.Persistence.Repositories.Abstraction.Interfaces;
+
+    public abstract class Repository<T, U> : IRepository<T, U> where T : BaseEntity<U>
+    {
+        protected readonly IUnitOfWork unitOfWork;
+
+        protected string tableName;
+        private StringBuilder stringBuilder;
+        protected string[] classPropertiesNames;
+        protected string columnNamesString;
+        protected string parameterizedColumnsNamesString;
+        protected string parameterizedColumnsNamesUpdateString;
+        protected string insertSqlCommand;
+        protected readonly string updateSqlCommand;
+        protected List<string> updateSkipProperties;
+
+        public Repository(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+            this.tableName = typeof(T).Name + 's';
+            this.classPropertiesNames = GetClassPropertiesNames();
+            this.columnNamesString = this.GetColumnsNamesString();
+            this.parameterizedColumnsNamesString = this.GetParameterizedColumnNamesString();
+
+            this.updateSkipProperties = new List<string>()
+            {
+                "Id",
+                "CreatedAtUtc",
+                "DeletedAtUtc",
+                "IsDeleted"
+            };
+
+            this.parameterizedColumnsNamesUpdateString = this.GetParameterizedColumnsNamesUpdateString();
+            this.insertSqlCommand = $"INSERT INTO {this.tableName} {this.columnNamesString} " +
+                                        $"VALUES {this.parameterizedColumnsNamesString}";
+            this.updateSqlCommand = $"UPDATE {this.tableName} SET {parameterizedColumnsNamesUpdateString} WHERE Id LIKE @Id AND " +
+                                    $"IsDeleted = 0";
+        }
+
+        public IDbConnection DbConnection => this.unitOfWork.DbConnection;
+
+        public IDbTransaction Transaction => this.unitOfWork.Transaction;
+
+        public T[] GetAll()
+        {
+            string sql = $"SELECT * FROM {this.tableName} WHERE IsDeleted = 0";
+            T[] entities = this.DbConnection.Query<T>(sql, transaction: this.Transaction).ToArray();
+
+            return entities;
+        }
+
+        public virtual T GetById(U id)
+        {
+            string sql = $"SELECT * FROM {this.tableName} WHERE Id = @Id AND IsDeleted = 0";
+            T entity = this.DbConnection.QueryFirstOrDefault<T>(sql, new { Id = id }, transaction: this.Transaction);
+
+            return entity;
+        }
+
+        public virtual void Create(T entity)
+        {
+            entity.CreatedAtUtc = DateTime.UtcNow;
+            entity.ModifiedAtUtc = entity.CreatedAtUtc;
+
+            this.DbConnection.Execute(this.insertSqlCommand, entity, transaction: this.Transaction);
+        }
+
+        public virtual void CreateMany(T[] entities)
+        {
+            foreach (T entity in entities)
+            {
+                entity.CreatedAtUtc = DateTime.UtcNow;
+                entity.ModifiedAtUtc = entity.CreatedAtUtc;
+            }
+
+            this.DbConnection.Execute(this.insertSqlCommand, entities, transaction: Transaction);
+        }
+
+
+        public virtual void Update(U id, T entity)
+        {
+            entity.Id = id;
+            entity.ModifiedAtUtc = DateTime.UtcNow;
+
+            string sql = $"UPDATE {this.tableName} SET {parameterizedColumnsNamesUpdateString} WHERE Id LIKE @Id AND IsDeleted = 0";
+            this.DbConnection.Execute(this.updateSqlCommand, entity, transaction: this.Transaction);
+        }
+
+        public virtual void Delete(U id)
+        {
+            string sql = $"UPDATE {this.tableName} SET IsDeleted = 1, DeletedAtUtc = GETDATE() WHERE Id = @Id";
+            this.DbConnection.Execute(sql, new { Id = id }, transaction: this.Transaction);
+        }
+
+        public virtual void DeleteMany(U[] ids)
+        {
+            string sql = $"UPDATE {this.tableName} SET IsDeleted = 1, DeletedAtUtc = GETDATE() WHERE Id IN @Id";
+
+
+            this.DbConnection.Execute(sql, new { Id = ids }, transaction: this.Transaction);
+        }
+
+        private static string[] GetClassPropertiesNames() => typeof(T).GetProperties().Select(p => p.Name).ToArray(); // Skip the Id
+
+        private string GetParameterizedColumnNamesString()
+        {
+            this.stringBuilder = new StringBuilder("(@");
+
+            this.stringBuilder.Append(string.Join(", @", this.classPropertiesNames));
+            this.stringBuilder.Append(')');
+
+            string tableColumnsNames = stringBuilder.ToString();
+            this.stringBuilder.Clear();
+
+            return tableColumnsNames;
+        }
+
+        private string GetColumnsNamesString()
+        {
+            this.stringBuilder = new StringBuilder("(");
+            this.stringBuilder.Append(string.Join(", ", classPropertiesNames));
+            this.stringBuilder.Append(')');
+
+            string columnNames = stringBuilder.ToString();
+            this.stringBuilder.Clear();
+
+            return columnNames;
+        }
+
+        private string GetParameterizedColumnsNamesUpdateString()
+        {
+            this.stringBuilder = new StringBuilder();
+
+            foreach (string propertyName in classPropertiesNames)
+            {
+                if (!this.updateSkipProperties.Contains(propertyName))
+                {
+                    this.stringBuilder.Append($"{propertyName}=@{propertyName}, ");
+                }
+            }
+
+            this.stringBuilder.Remove(stringBuilder.Length - 2, 2);
+
+            string parameterizedColumnsNamesUpdateString = this.stringBuilder.ToString();
+            this.stringBuilder.Clear();
+
+            return parameterizedColumnsNamesUpdateString;
+        }
+    }
+}
