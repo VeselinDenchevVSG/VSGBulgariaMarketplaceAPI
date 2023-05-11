2f4d69075938e1cbd54db03d29451ffac6fce5f0
Veselin Denchev <VDenchev@vsgbg.com>
2023-05-11 16:50:37 +0300

    Implement cache for ItemService and OrderService

--- src/Core/VSGBulgariaMarketplace.Application/Services/ItemService.cs
+++ src/Core/VSGBulgariaMarketplace.Application/Services/ItemService.cs
@@ -1,55 +1,92 @@
 ﻿namespace VSGBulgariaMarketplace.Application.Services
 {
     using AutoMapper;
-    using Microsoft.Extensions.Caching.Memory;
 
     using VSGBulgariaMarketplace.Application.Models.Item.Dtos;
     using VSGBulgariaMarketplace.Application.Models.Item.Interfaces;
+    using VSGBulgariaMarketplace.Application.Services.HelpServices.Interfaces;
     using VSGBulgariaMarketplace.Domain.Entities;
 
     public class ItemService : BaseService<IItemRepository, Item>, IItemService
     {
-        public ItemService(IItemRepository itemRepository, IMemoryCache memoryCache, IMapper mapper)
-            : base(itemRepository, memoryCache, mapper)
+        internal const string MARKETPLACE_CACHE_KEY = "marketplace";
+        internal const string INVENTORY_CACHE_KEY = "inventory";
+        internal const string ITEM_CACHE_KEY_TEMPLATE = "item-{0}";
+
+        public ItemService(IItemRepository itemRepository, IMemoryCacheAdapter cacheAdapter, IMapper mapper)
+            : base(itemRepository, cacheAdapter, mapper)
         {
         }
 
         public MarketplaceItemDto[] GetMarketplace()
         {
-            Item[] items = this.repository.GetMarketplace();
-            MarketplaceItemDto[] itemDtos = base.mapper.Map<MarketplaceItemDto[]>(items);
+            MarketplaceItemDto[] itemDtos = base.cacheAdapter.Get<MarketplaceItemDto[]>(MARKETPLACE_CACHE_KEY);
+            if (itemDtos is null)
+            {
+                Item[] items = this.repository.GetMarketplace();
+                itemDtos = base.mapper.Map<MarketplaceItemDto[]>(items);
+
+                base.cacheAdapter.Set(MARKETPLACE_CACHE_KEY, itemDtos);
+            }
 
             return itemDtos;
         }
 
         public InventoryItemDto[] GetInventory()
         {
-            Item[] items = this.repository.GetInventory();
-            InventoryItemDto[] itemDtos = base.mapper.Map<InventoryItemDto[]>(items);
+            InventoryItemDto[] itemDtos = base.cacheAdapter.Get<InventoryItemDto[]>(INVENTORY_CACHE_KEY);
+            if (itemDtos is null)
+            {
+                Item[] items = this.repository.GetInventory();
+                itemDtos = base.mapper.Map<InventoryItemDto[]>(items);
+
+                base.cacheAdapter.Set(INVENTORY_CACHE_KEY, itemDtos);
+            }
 
             return itemDtos;
         }
 
         public ItemDetailsDto GetByCode(int code)
         {
-            Item item = this.repository.GetByCode(code);
-            ItemDetailsDto itemDto = base.mapper.Map<Item, ItemDetailsDto>(item);
+            string itemCacheKey = string.Format(ITEM_CACHE_KEY_TEMPLATE, code);
+
+            ItemDetailsDto itemDto = base.cacheAdapter.Get<ItemDetailsDto>(itemCacheKey);
+            if (itemDto is null)
+            {
+                Item item = this.repository.GetByCode(code);
+                itemDto = base.mapper.Map<Item, ItemDetailsDto>(item);
+
+                base.cacheAdapter.Set(itemCacheKey, itemDto);
+            }
 
             return itemDto;
         }
 
         public void Create(ManageItemDto createItemDto)
         {
+            base.cacheAdapter.Remove(MARKETPLACE_CACHE_KEY);
+            base.cacheAdapter.Remove(INVENTORY_CACHE_KEY);
+
             Item item = base.mapper.Map<ManageItemDto, Item>(createItemDto);
             this.repository.Create(item);
         }
 
         public void Update(int code, ManageItemDto updateItemDto) 
         {
+            base.cacheAdapter.Clear();
+
             Item item = base.mapper.Map<ManageItemDto, Item>(updateItemDto);
+
             this.repository.Update(code, item);
         }
 
-        public void Delete(int id) => base.repository.Delete(id);
+        public void Delete(int code)
+        {
+            base.cacheAdapter.Remove(MARKETPLACE_CACHE_KEY);
+            base.cacheAdapter.Remove(INVENTORY_CACHE_KEY);
+            base.cacheAdapter.Remove(string.Format(ITEM_CACHE_KEY_TEMPLATE, code));
+
+            base.repository.Delete(code);
+        }    
     }
 }
--- src/Core/VSGBulgariaMarketplace.Application/Services/OrderService.cs
+++ src/Core/VSGBulgariaMarketplace.Application/Services/OrderService.cs
@@ -2,41 +2,61 @@
 {
     using AutoMapper;
 
-    using Microsoft.Extensions.Caching.Memory;
-
     using VSGBulgariaMarketplace.Application.Models.Item.Interfaces;
     using VSGBulgariaMarketplace.Application.Models.Order.Dtos;
     using VSGBulgariaMarketplace.Application.Models.Order.Interfaces;
+    using VSGBulgariaMarketplace.Application.Services.HelpServices.Interfaces;
     using VSGBulgariaMarketplace.Domain.Entities;
 
     public class OrderService : BaseService<IOrderRepository, Order>, IOrderService
     {
+        public const string PENDING_ORDERS_CACHE_KEY = "pending-orders";
+        private const string USER_ORDER_CACHE_KEY_TEMPLATE = "orders-user-{0}";
+        private const string FAKE_USER_ID = "1234"; // For debugging purposes I will use fake user id until we implement authentication
+
         private IItemRepository itemRepository;
 
-        public OrderService(IOrderRepository repository, IItemRepository itemRepository, IMemoryCache memoryCache, IMapper mapper) 
-            : base(repository, memoryCache, mapper)
+        public OrderService(IOrderRepository repository, IItemRepository itemRepository, IMemoryCacheAdapter cacheAdapter, IMapper mapper) 
+            : base(repository, cacheAdapter, mapper)
         {
             this.itemRepository = itemRepository;
         }
 
         public PendingOrderDto[] GetPendingOrders()
         {
-            Order[] pendingOrders = base.repository.GetPendingOrders();
-            PendingOrderDto[] pendingOrderDtos = base.mapper.Map<Order[], PendingOrderDto[]>(pendingOrders);
+            PendingOrderDto[] pendingOrderDtos = base.cacheAdapter.Get<PendingOrderDto[]>(PENDING_ORDERS_CACHE_KEY);
+            if (pendingOrderDtos is null)
+            {
+                Order[] pendingOrders = base.repository.GetPendingOrders();
+                pendingOrderDtos = base.mapper.Map<Order[], PendingOrderDto[]>(pendingOrders);
+
+                base.cacheAdapter.Set(PENDING_ORDERS_CACHE_KEY, pendingOrderDtos);
+            }
 
             return pendingOrderDtos;
         }
 
         public UserOrderDto[] GetUserOrders(string userId)
         {
-            Order[] userOrders = base.repository.GetUserOrders(userId);
-            UserOrderDto[] userOrderDtos = base.mapper.Map<Order[], UserOrderDto[]>(userOrders);
+            string userOrdersCacheKey = string.Format(USER_ORDER_CACHE_KEY_TEMPLATE, FAKE_USER_ID);
+
+            UserOrderDto[] userOrderDtos = base.cacheAdapter.Get<UserOrderDto[]>(userOrdersCacheKey);
+            if (userOrderDtos is null)
+            {
+                Order[] userOrders = base.repository.GetUserOrders(userId);
+                userOrderDtos = base.mapper.Map<Order[], UserOrderDto[]>(userOrders);
+
+                base.cacheAdapter.Set(userOrdersCacheKey, userOrderDtos);
+            }
 
             return userOrderDtos;
         }
 
         public void Create(CreateOrderDto orderDto)
         {
+            base.cacheAdapter.Remove(PENDING_ORDERS_CACHE_KEY);
+            base.cacheAdapter.Remove(string.Format(USER_ORDER_CACHE_KEY_TEMPLATE, FAKE_USER_ID));
+
             Order order = base.mapper.Map<CreateOrderDto, Order>(orderDto);
 
             Item item = this.itemRepository.GetQuantityForSaleAndPriceByCode(orderDto.ItemCode);
@@ -55,11 +75,24 @@
         public void Finish(int id)
         {
             Order order = base.repository.GetOrderItemIdAndQuantity(id);
+
+            base.cacheAdapter.Remove(ItemService.MARKETPLACE_CACHE_KEY);
+            base.cacheAdapter.Remove(ItemService.INVENTORY_CACHE_KEY);
+            base.cacheAdapter.Remove(string.Format(ItemService.ITEM_CACHE_KEY_TEMPLATE, order.ItemId));
+            base.cacheAdapter.Remove(PENDING_ORDERS_CACHE_KEY);
+            base.cacheAdapter.Remove(string.Format(USER_ORDER_CACHE_KEY_TEMPLATE, FAKE_USER_ID));
+
             this.itemRepository.BuyItem(order.ItemId, order.Quantity);
 
             base.repository.Finish(id);
         }
 
-        public void Decline(int id) => base.repository.Delete(id);
+        public void Decline(int id)
+        {
+            base.cacheAdapter.Remove(PENDING_ORDERS_CACHE_KEY);
+            base.cacheAdapter.Remove(string.Format(USER_ORDER_CACHE_KEY_TEMPLATE, FAKE_USER_ID));
+
+            base.repository.Delete(id);
+        }
     }
 }
