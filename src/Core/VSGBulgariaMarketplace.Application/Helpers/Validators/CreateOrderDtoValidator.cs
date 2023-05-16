8c397319101f55d26bd47b5759aac79cf4bfbfa0
Veselin Denchev <VDenchev@vsgbg.com>
2023-05-16 10:36:39 +0300

    Create CreateOrderDtoValidator.cs

---
+++ src/Core/VSGBulgariaMarketplace.Application/Helpers/Validators/CreateOrderDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace VSGBulgariaMarketplace.Application.Helpers.Validators
+{
+    using FluentValidation;
+
+    using VSGBulgariaMarketplace.Application.Models.Order.Dtos;
+
+    internal class CreateOrderDtoValidator : AbstractValidator<CreateOrderDto>
+    {
+        public CreateOrderDtoValidator()
+        {
+            RuleFor(o => o.ItemCode).InclusiveBetween(1, int.MaxValue).WithMessage($"Item code must be between 1 and {int.MaxValue}!");
+            RuleFor(o => o.Quantity).InclusiveBetween((short) 1, short.MaxValue).WithMessage($"Order quantity must be between 1 and {short.MaxValue}!");
+        }
+    }
+}
