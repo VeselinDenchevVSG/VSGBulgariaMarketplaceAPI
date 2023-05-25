5f3e80a1d68e19b97b82ff658986f64207a47996
Veselin Denchev <VDenchev@vsgbg.com>
2023-05-25 13:24:55 +0300

    Fix Order profile to map OrderDate to DateTime type

--- src/Core/VSGBulgariaMarketplace.Application/Helpers/MapProfiles/OrderProfile.cs
+++ src/Core/VSGBulgariaMarketplace.Application/Helpers/MapProfiles/OrderProfile.cs
@@ -10,9 +10,9 @@
         public OrderProfile()
         {
             CreateMap<Order, PendingOrderDto>().ForMember(dto => dto.ItemCode, x => x.MapFrom(e => e.ItemId))
-                                                .ForMember(dto => dto.OrderDate, x => x.MapFrom(e => e.CreatedAtUtc.ToLocalTime().ToShortTimeString()));
+                                                .ForMember(dto => dto.OrderDate, x => x.MapFrom(e => e.CreatedAtUtc.ToLocalTime()));
             CreateMap<Order, UserOrderDto>().ForMember(dto => dto.ItemName, x => x.MapFrom(e => e.Item.Name))
-                                            .ForMember(dto => dto.OrderDate, x => x.MapFrom(e => e.CreatedAtUtc.ToLocalTime().ToShortTimeString()))
+                                            .ForMember(dto => dto.OrderDate, x => x.MapFrom(e => e.CreatedAtUtc.ToLocalTime()))
                                             .ForMember(dto => dto.Status, x => x.MapFrom(e => e.Status.ToString()));
             CreateMap<CreateOrderDto, Order>().ForMember(e => e.ItemId, x => x.MapFrom(dto => dto.ItemCode))
                                                 .ForMember(e => e.Status, x => x.MapFrom(dto => OrderStatus.Pending));
