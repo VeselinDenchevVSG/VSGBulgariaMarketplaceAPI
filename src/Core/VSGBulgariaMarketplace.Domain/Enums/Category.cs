628f26b46ee0f438de0df8ae62b7d9a17d099c2a
Veselin Denchev <VDenchev@vsgbg.com>
2023-05-31 17:51:50 +0300

    Add new item categories

--- src/Core/VSGBulgariaMarketplace.Domain/Enums/Category.cs
+++ src/Core/VSGBulgariaMarketplace.Domain/Enums/Category.cs
@@ -1,7 +1,16 @@
 ﻿namespace VSGBulgariaMarketplace.Domain.Enums
 {
+    using System.ComponentModel.DataAnnotations;
+
     public enum Category
     {
-        Laptops = 1
+        Laptops = 1,
+
+        Furniture = 2,
+
+        [Display(Name = "Office tools")]
+        OfficeTools = 3,
+
+        Misc = 4
     }
 }
