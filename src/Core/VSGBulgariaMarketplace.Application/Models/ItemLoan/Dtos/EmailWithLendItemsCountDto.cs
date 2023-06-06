a75a3f4f4f0e487a1ced2e57d0053ae62ee1b0bc
Veselin Denchev <VDenchev@vsgbg.com>
2023-06-06 11:41:18 +0300

    Add [JsonPropertyName("count")] attribute to LendItemsCount property

--- src/Core/VSGBulgariaMarketplace.Application/Models/ItemLoan/Dtos/EmailWithLendItemsCountDto.cs
+++ src/Core/VSGBulgariaMarketplace.Application/Models/ItemLoan/Dtos/EmailWithLendItemsCountDto.cs
@@ -1,9 +1,12 @@
 ﻿namespace VSGBulgariaMarketplace.Application.Models.ItemLoan.Dtos
 {
+    using System.Text.Json.Serialization;
+
     public class EmailWithLendItemsCountDto
     {
         public string Email { get; set; }
 
+        [JsonPropertyName("count")]
         public int LendItemsCount { get; set; }
     }
 }
