2e49f60fcef4ceacce09240cf5d0c26373b53733
Veselin Denchev <VDenchev@vsgbg.com>
2023-06-06 09:26:15 +0300

    Add [JsonPropertyName("orderedBy")] attribute to Email property

--- src/Core/VSGBulgariaMarketplace.Application/Models/ItemLoan/Dtos/LendItemsDto.cs
+++ src/Core/VSGBulgariaMarketplace.Application/Models/ItemLoan/Dtos/LendItemsDto.cs
@@ -1,11 +1,14 @@
 ﻿namespace VSGBulgariaMarketplace.Application.Models.ItemLoan.Dtos
 {
+    using System.Text.Json.Serialization;
+
     public class LendItemsDto
     {
         //public string ItemId { get; set; }
 
         public short Quantity { get; set; }
 
+        [JsonPropertyName("orderedBy")]
         public string Email { get; set; }
     }
 }
