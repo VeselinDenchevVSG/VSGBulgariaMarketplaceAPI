03f415628f546b7795d752d29718c8b3acec4c44
Veselin Denchev <VDenchev@vsgbg.com>
2023-06-09 11:10:51 +0300

    Fix migration for adding AvailableQuantity column in Items table
    Add constant for constraint name. Fix wrong table name in Down() method

--- src/Infrastructure/VSGBulgariaMarketplace.Persistence/Migrations/202306021402_AddAvailableQuantityColumnToItemsTable.cs
+++ src/Infrastructure/VSGBulgariaMarketplace.Persistence/Migrations/202306021402_AddAvailableQuantityColumnToItemsTable.cs
@@ -7,17 +7,18 @@
     {
         private const string ITEMS_TABLE_NAME = "Items";
         private const string AVAILABLE_QUANTITY_COLUMN_NAME = "AvailableQuantity";
+        private const string AVAILABLE_QUANTITY_CHECK_CONSTRAINT_NAME = "CK_Item_AvailableQuantity";
 
         public override void Up()
         {
             Alter.Table(ITEMS_TABLE_NAME).AddColumn(AVAILABLE_QUANTITY_COLUMN_NAME).AsInt16().Nullable();
 
-            Execute.Sql($"ALTER TABLE {ITEMS_TABLE_NAME} ADD CONSTRAINT CK_Item_AvailableQuantity CHECK (AvailableQuantity >= 0)");
+            Execute.Sql($"ALTER TABLE {ITEMS_TABLE_NAME} ADD CONSTRAINT {AVAILABLE_QUANTITY_CHECK_CONSTRAINT_NAME} CHECK ({AVAILABLE_QUANTITY_COLUMN_NAME} >= 0)");
         }
 
         public override void Down()
         {
-            Execute.Sql($"ALTER TABLE Persons DROP CONSTRAINT CK_Item_AvailableQuantity");
+            Execute.Sql($"ALTER TABLE {ITEMS_TABLE_NAME} DROP CONSTRAINT {AVAILABLE_QUANTITY_CHECK_CONSTRAINT_NAME}");
 
             Delete.Column(AVAILABLE_QUANTITY_COLUMN_NAME).FromTable(ITEMS_TABLE_NAME);
         }
