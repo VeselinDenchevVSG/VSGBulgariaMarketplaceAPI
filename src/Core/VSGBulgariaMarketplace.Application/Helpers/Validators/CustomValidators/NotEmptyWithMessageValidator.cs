8678c87a5378d85b1173402354433a3052b4ad16
Veselin Denchev <VDenchev@vsgbg.com>
2023-06-16 17:27:48 +0300

    Fix NotEmptyWithMessageValidator message not being formatted properly

--- src/Core/VSGBulgariaMarketplace.Application/Helpers/Validators/CustomValidators/NotEmptyWithMessageValidator.cs
+++ src/Core/VSGBulgariaMarketplace.Application/Helpers/Validators/CustomValidators/NotEmptyWithMessageValidator.cs
@@ -6,50 +6,49 @@
 
     internal static class NotEmptyWithMessageValidator
     {
-        public static IRuleBuilderOptions<T, string> NotEmptyWithMessage<T, TElement, TEntity>(this IRuleBuilder<T, string> ruleBuilder, 
-                                                                                                string message = ValidationConstant.NOT_EMPTY_ERROR_MESSAGE)
+        public static IRuleBuilderOptions<T, string> NotEmptyWithMessage<T, TElement, TEntity>(this IRuleBuilder<T, string> ruleBuilder, string propertyName, string message = null)
         {
-            if (message == ValidationConstant.NOT_EMPTY_ERROR_MESSAGE)
-            {
-                message = FormatNotEmptyMessage(typeof(TEntity));
-            }
+            message = NotEmptyWithMessageBase<TEntity>(propertyName, message);
 
             return ruleBuilder.NotEmpty().WithMessage(message);
         }
 
-        public static IRuleBuilderOptions<T, int> NotEmptyWithMessage<T, TElement, TEntity>(this IRuleBuilder<T, int> ruleBuilder,
-                                                                                                string message = ValidationConstant.NOT_EMPTY_ERROR_MESSAGE)
+        public static IRuleBuilderOptions<T, int> NotEmptyWithMessage<T, TElement, TEntity>(this IRuleBuilder<T, int> ruleBuilder, string propertyName, string message = null)
         {
-            if (message == ValidationConstant.NOT_EMPTY_ERROR_MESSAGE)
-            {
-                message = FormatNotEmptyMessage(typeof(TEntity));
-            }
+            message = NotEmptyWithMessageBase<TEntity>(propertyName, message);
 
             return ruleBuilder.NotEmpty().WithMessage(message);
         }
 
-        public static IRuleBuilderOptions<T, short> NotEmptyWithMessage<T, TElement, TEntity>(this IRuleBuilder<T, short> ruleBuilder, 
-                                                                                                string message = ValidationConstant.NOT_EMPTY_ERROR_MESSAGE)
+        public static IRuleBuilderOptions<T, short> NotEmptyWithMessage<T, TElement, TEntity>(this IRuleBuilder<T, short> ruleBuilder, string propertyName, string message = null)
         {
-            if (message == ValidationConstant.NOT_EMPTY_ERROR_MESSAGE)
-            {
-                message = FormatNotEmptyMessage(typeof(TEntity));
-            }
+            message = NotEmptyWithMessageBase<TEntity>(propertyName, message);
 
             return ruleBuilder.NotEmpty().WithMessage(message);
         }
 
-        public static IRuleBuilderOptions<T, decimal> NotEmptyWithMessage<T, TElement, TEntity>(this IRuleBuilder<T, decimal> ruleBuilder, 
-                                                                                                string message = ValidationConstant.NOT_EMPTY_ERROR_MESSAGE)
+        public static IRuleBuilderOptions<T, decimal?> NotEmptyWithMessage<T, TElement, TEntity>(this IRuleBuilder<T, decimal?> ruleBuilder, string propertyName, string message = null)
         {
-            if (message == ValidationConstant.NOT_EMPTY_ERROR_MESSAGE)
-            {
-                message = FormatNotEmptyMessage(typeof(TEntity));
-            }
+            message = NotEmptyWithMessageBase<TEntity>(propertyName, message);
 
             return ruleBuilder.NotEmpty().WithMessage(message);
         }
 
-        private static string FormatNotEmptyMessage(Type type) => $"{type.Name} {ValidationConstant.NOT_EMPTY_ERROR_MESSAGE}";
+        private static string NotEmptyWithMessageBase<TEntity>(string propertyName, string message = null)
+        {
+            if (message is null)
+            {
+                message = FormatNotEmptyMessage(typeof(TEntity), propertyName);
+            }
+
+            return message;
+        }
+
+        private static string FormatNotEmptyMessage(Type type, string propertyName)
+        {
+            string message = string.Format(ValidationConstant.NOT_EMPTY_ERROR_MESSAGE,$"{type.Name} {propertyName}");
+
+            return message;
+        }
     }
 }
